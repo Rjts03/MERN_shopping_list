a2a8493c6ffceb939a43c5affe34c0740d38ab5b
Rajat Shah <srajat0301@gmail.com>
2019-05-08 09:18:46 +0530

    Writing the server side code + integrating mongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://rajat:mongorajat03@shoppinglist-o8bxi.mongodb.net/test?retryWrites=true'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparder MW
+app.use(bodyParser.json());
+
+// DB Config
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('mongoDB connected'))
+  .catch(e => console.log('mongoERR', e));
+
+// Use Routes
+app.use('/api/items', items);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
